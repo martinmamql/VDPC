b9300abf4d9b2dff948d0f8a39e8ef4b5c2c534a
mratinmamql <qianlim@t1v-n-05ee0df0-w-0.us-central2-b.c.tpup-381616.internal>
2023-05-05 12:49:34 +0000

    Automatically download celeba

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@ pip install --upgrade pip
 #install hparams
 pip install --upgrade git+https://github.com/Rayhane-mamah/hparams
 
-pip install -r requirements.txt
-
 # Installs the wheel compatible with Cuda >= 11.1 and cudnn >= 8.0.5
 #pip install jax[cuda11_cudnn805] -f https://storage.googleapis.com/jax-releases/jax_releases.html
 
@@ -17,3 +15,6 @@ pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_relea
 
 # Install JAX extensions
 pip install --upgrade optax flax trax
+
+pip install -r requirements.txt
+
